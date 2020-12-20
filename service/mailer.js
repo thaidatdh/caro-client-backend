82b35eb2e58002fb36ccc15b539f79ff4c5d203f
Dat Ho <thaidatdh@gmail.com>
2020-12-20 11:35:46 +0700

    update mail config

--- service/mailer.js
+++ service/mailer.js
@@ -1,18 +1,19 @@
 let nodemailer = require("nodemailer");
 exports.sendMail = async function (email, header, content) {
   const fromEmail = {
-    email: "gmail-go-here@gmail.com",
-    pwd: "password-go-here",
+    email: "ptudw1733@gmail.com",
+    pwd1: "PTUDW",
+    pwd2: "1733@",
   };
   let transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
       user: fromEmail.email,
-      pass: fromEmail.pwd,
+      pass: fromEmail.pwd1 + fromEmail.pwd2,
     },
   });
   var mailOptions = {
-    from: '"CaroTeam" <' + fromEmail.email + ">",
+    from: '"CaroTeam" <' + fromEmail.email + '>',
     to: email,
     subject: "[Caro] " + header,
     text: content,
